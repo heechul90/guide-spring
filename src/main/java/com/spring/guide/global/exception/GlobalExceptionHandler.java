dafc8d5cae8e588805ab008658d5751765084640
heechul <heechul4296@gmail.com>
2023-02-01 00:00:32 +0900

    global error handling 추가중...

--- src/main/java/com/spring/guide/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/spring/guide/global/exception/GlobalExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.spring.guide.global.exception;
 
+import com.spring.guide.global.exception.dto.ErrorCode;
+import com.spring.guide.global.exception.dto.ErrorResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,9 +20,28 @@ public class GlobalExceptionHandler {
 
     private final MessageSource messageSource;
 
+    /**
+     * javax.validation.Valid or @Validated 으로 binding error 발생시 발생한다.
+     * HttpMessageConverter 에서 등록한 HttpMessageConverter binding 못할경우 발생
+     * 주로 @RequestBody, @RequestPart 어노테이션 발생
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    protected ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        return null;
+    protected Mono<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("handleMethodArgumentNotValidException", e);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult(), messageSource);
+        return Mono.just(response);
+    }
+
+    /**
+     * 바인딩 객체 @ModelAttribute 으로 binding error 발생시 BindException 발생한다.
+     * ref https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-ann-modelattrib-method-args
+     */
+    @ExceptionHandler(BindException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected  Mono<ErrorResponse> handleBindException(BindException e) {
+        log.error("handleBindException", e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult(), messageSource);
+        return Mono.just(response);
     }
 }
